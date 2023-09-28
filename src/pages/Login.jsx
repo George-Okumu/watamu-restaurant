2f7de7994c5bf4de3880bf8d39674ac4bb9d4564
George-Okumu <gokumu12@gmail.com>
2023-09-28 22:10:26 +0300

    user able to login, token stored in web storage for routes that need it

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,48 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
+  const history = useNavigate();
+
+  const [loginData, setLoginData] = useState({
+    "username": "",
+    "password": "",
+  });
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+      try {
+        let loginres = await fetch("http://localhost:5000/login", {
+          method: "POST",
+          headers: {
+            // "Authorization": `Bearer ${mytoken}`,
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(loginData),
+        })
+
+        if(loginres.ok){
+          let re = await loginres.json();
+          localStorage.setItem("loginToken", re.access_token);
+          history("/products");
+
+        }else{
+          console.log("Maybe I dont know, query the errors later")
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    
+  };
+
+  const handleLoginInputChange = (e) => {
+    setLoginData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   return (
     <>
       <section>
@@ -19,39 +60,34 @@ function Login() {
             </div>
 
             <div className="mb-12 mr-4 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
-              <form>
-                <div className="relative mb-6" data-te-input-wrapper-init>
+              <form onSubmit={handleLogin}>
+                <div className="relative mb-6">
                   <input
                     type="text"
-                    className="peer block min-h-[auto] w-full rounded border-0 bg-yellow-200 px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                    id="password"
+                    className="peer block min-h-[auto] w-full rounded border-0 bg-yellow-200 px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100"
+                    id="username"
+                    onChange={handleLoginInputChange}
+                    name="username"
+                    value={loginData.username}
                     placeholder="Username"
                   />
-                  <label
-                    for="Username"
-                    className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-yellow-500 peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-yellow-500">
-                    Username
-                  </label>
                 </div>
 
-                <div className="relative mb-6" data-te-input-wrapper-init>
+                <div className="relative mb-6">
                   <input
                     type="password"
-                    className="peer block min-h-[auto] w-full rounded border-0 bg-yellow-200 px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                    className="peer block min-h-[auto] w-full rounded border-0 bg-yellow-200 px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100"
                     id="password"
+                    name="password"
+                    value={loginData.password}
+                    onChange={handleLoginInputChange}
                     placeholder="Password"
                   />
-                  <label
-                    for="password"
-                    className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-yellow-500 peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-yellow-500">
-                    Password
-                  </label>
                 </div>
-
 
                 <div className="text-center lg:text-left">
                   <button
-                    type="button"
+                    type="submit"
                     className="inline-block rounded bg-yellow-300 px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-yellow-500-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-yellow-500-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-yellow-500-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                     data-te-ripple-init
                     data-te-ripple-color="light">
@@ -60,7 +96,8 @@ function Login() {
 
                   <p className="mb-0 mt-2 pt-1 text-sm font-semibold">
                     Don't have an account?
-                    <Link to="/register"
+                    <Link
+                      to="/register"
                       className="text-danger hover:text-yellow-500 transition duration-150 ease-in-out hover:text-danger-600 focus:text-danger-600 active:text-danger-700">
                       Register
                     </Link>
