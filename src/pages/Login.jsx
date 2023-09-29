bc81d0fb4ae9e26a104f9fff4113c37cb6deb75f
George-Okumu <gokumu12@gmail.com>
2023-09-29 22:54:24 +0300

    handled the errors for the client side

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,28 +1,27 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Spinner from "../components/Spinner";
-import sweetalert2 from "sweetalert2";
 import { AuthContext } from "../components/AuthContext";
 
 function Login() {
   const navigate = useNavigate();
-
   const [loginData, setLoginData] = useState({
     username: "",
     password: "",
   });
   const [isLoading, setIsLoading] = useState(false);
+  const [isLoginError, setIsLoginError] = useState(false);
+  const [isServerError, setIsServerError] = useState(false);
+
   // const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
 
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    setIsLoading(true);
     try {
       let loginres = await fetch("http://localhost:5000/login", {
         method: "POST",
         headers: {
-          // "Authorization": `Bearer ${mytoken}`,
           "Content-Type": "application/json",
         },
         body: JSON.stringify(loginData),
@@ -32,24 +31,27 @@ function Login() {
         let re = await loginres.json();
         localStorage.setItem("loginToken", re.access_token); // sets token to local storage
 
+        setIsLoading(true);
         setTimeout(() => {
-          // Perform login logic, e.g., validate email and password
-          // Assuming successful login for demonstration purposes
-          sweetalert2.fire({
-            title: "Success!",
-            text: "Login Successfull",
-            icon: "success",
-            // confirmButtonText: "Cool",
-          });
+          setIsLoading(false);
           navigate("/products");
-        }, 1500);
+        }, 2000);
+
         // setIsAuthenticated(true);
         // setIsLoading(false);
       } else {
-        console.log("Maybe I dont know, query the errors later");
+        setTimeout(() => {
+          setIsLoading(false);
+          setIsLoginError(true);
+        }, 2000);
       }
     } catch (error) {
-      console.log(error);
+      setIsLoading(true);
+
+      setTimeout(() => {
+        setIsLoading(false);
+        setIsServerError(true);
+      }, 1000)
     }
   };
 
@@ -103,14 +105,30 @@ function Login() {
                 </div>
 
                 <div className="text-center lg:text-left">
-                  <button
-                    type="submit"
-                    className="inline-block rounded bg-yellow-300 px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-yellow-500-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-yellow-500-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-yellow-500-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                    data-te-ripple-init
-                    data-te-ripple-color="light">
-                    Login
-                  </button>
+                  {isLoginError ? (
+                    <p class="text-red-500 text-sm pb-4 italic">
+                      Invalid Credentials. Wrong Username or Password.Try Again.
+                    </p>
+                  ) : isServerError ? (
+                    <p class="text-red-500 text-sm pb-4 italic">
+                      Network connection error, if persists, kindly contact us.
+                      <em>Make sure you are connected to internet.</em>
+                    </p>
+                  ) : (
+                    <p></p>
+                  )}
 
+                  {isLoading ? (
+                    <Spinner />
+                  ) : (
+                    <button
+                      type="submit"
+                      className="inline-block rounded bg-yellow-300 px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-yellow-500-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-yellow-500-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-yellow-500-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                      data-te-ripple-init
+                      data-te-ripple-color="light">
+                      Login
+                    </button>
+                  )}
                   <p className="mb-0 mt-2 pt-1 text-sm font-semibold">
                     Don't have an account?
                     <Link
